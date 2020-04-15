98a50f3ad7f00f5720785fd0299b2bfda1aa6265
NhaDev <ducnha7791@gmail.com>
2020-04-15 11:19:57 +0700

    Test Git Commit 1

---
+++ GitSample2020.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitSample2020._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
